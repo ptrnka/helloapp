dc89a8d232838fa7a810a81faafbc0d337f90b77
white <ptrnkaa@gmail.com>
2021-11-03 19:43:47 +0500

    Hello white

--- helloapp/helloapp/Program.cs
+++ helloapp/helloapp/Program.cs
@@ -8,5 +8,10 @@ namespace helloapp
         {
             Console.WriteLine("hello");
         }
+
+        public static void greetWhite()
+        {
+            Console.WriteLine("hello white");
+        }
     }
 }
